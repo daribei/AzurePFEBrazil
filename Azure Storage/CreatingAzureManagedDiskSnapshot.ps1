613f012262e702a054721d16903ba0d161851607
daribei <danielribeiro95@outlook.com.br>
2020-02-12 14:36:06 -0300

    New script added.

---
+++ Azure Storage/CreatingAzureManagedDiskSnapshot.ps1
@@ -0,0 +1,56 @@
+Param(
+    [string]$resourceGroupName
+)
+
+# Ensures you do not inherit an AzureRMContext in your runbook
+#Disable-AzureRmContextAutosave –Scope Process
+
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+while (!($connectionResult) -And ($logonAttempt -le 10)) {
+    $LogonAttempt++
+    # Logging in to Azure...
+    $connectionResult = Connect-AzAccount `
+        -ServicePrincipal `
+        -Tenant $connection.TenantID `
+        -ApplicationID $connection.ApplicationID `
+        -CertificateThumbprint $connection.CertificateThumbprint
+
+    Start-Sleep -Seconds 30
+}
+
+foreach ($VMs in Get-AzVM -ResourceGroupName $resourceGroupName) {  
+    #Set local variables 
+    $location = $VMs.Location 
+    #$resourceGroupName = $vmInfo.ResourceGroupName 
+    $timestamp = Get-Date -f MM-dd-yyyy_HH_mm_ss 
+ 
+    #Snapshot name of OS data disk 
+    $snapshotName = $VMs.Name + $timestamp  
+ 
+    #Create snapshot configuration 
+    $snapshot = New-AzSnapshotConfig -SourceUri $VMs.StorageProfile.OsDisk.ManagedDisk.Id -Location $location  -CreateOption copy 
+                 
+    #Take snapshot 
+    New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName  
+                 
+
+                 
+    if ($VMs.StorageProfile.DataDisks.Count -ge 1) { 
+        #Condition with more than one data disks 
+        for ($i = 0; $i -le $VMs.StorageProfile.DataDisks.Count - 1; $i++) { 
+                                 
+            #Snapshot name of OS data disk 
+            $snapshotName = $VMs.StorageProfile.DataDisks[$i].Name + $timestamp  
+                             
+            #Create snapshot configuration 
+            $snapshot = New-AzSnapshotConfig -SourceUri $VMs.StorageProfile.DataDisks[$i].ManagedDisk.Id -Location $location  -CreateOption copy 
+                             
+            #Take snapshot 
+            New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName  
+                             
+        } 
+    } 
+    else { 
+        Write-Host $VMs.Name + " doesn't have any additional data disk." 
+    } 
+}
