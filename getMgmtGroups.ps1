2404f509126d2869648de99956608258266e8313
Daniel Ribeiro <34457162+daribei@users.noreply.github.com>
2020-05-27 16:13:14 -0300

    Add files via upload

---
+++ getMgmtGroups.ps1
@@ -0,0 +1,9 @@
+﻿$mgmtgroups = Get-AzManagementGroup
+
+foreach ($mgmtgroup in $mgmtgroups)
+{
+    if ($mgmtgroup.DisplayName -ne "Tenant Root Group")
+    {
+        Get-AzManagementGroup -GroupName $mgmtgroup.DisplayName -Expand -Recurse
+    }
+}
