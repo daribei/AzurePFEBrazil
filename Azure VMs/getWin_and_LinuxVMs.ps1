74bb18749d18dbcc492d55c5c4a5c2d8cab73cae
Daniel Ribeiro <34457162+daribei@users.noreply.github.com>
2020-08-25 17:10:39 -0300

    Add files via upload

---
+++ Azure VMs/getWin_and_LinuxVMs.ps1
@@ -0,0 +1,22 @@
+﻿$Linux = 0
+$Windows = 0
+
+foreach ($Subscription in (Get-AzSubscription))
+{
+    Select-AzSubscription -SubscriptionId ($Subscription).Id
+
+        foreach ($vms in (Get-AzVM))
+        {      
+            if ($vm.StorageProfile.OsDisk.OsType -eq "Linux" )
+                {
+                    $Linux++
+                }
+            else
+                {
+                    $Windows++   
+                }
+        }
+}
+
+Write-Host "VMs Linux: " $Linux
+Write-Host "VMs Windows: " $Windows
