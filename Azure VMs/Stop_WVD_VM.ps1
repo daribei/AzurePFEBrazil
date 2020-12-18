5152fc257d7fc563772e71d1c24fdafe6427f8dd
Daniel Ribeiro <34457162+daribei@users.noreply.github.com>
2020-12-18 17:38:33 -0300

    Add files via upload

---
+++ Azure VMs/Stop_WVD_VM.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [Parameter(Mandatory = $True)]
+    [String] $email
+)
+
+$hostPoolRG = "Host Pool - RG Name"
+$hostPoolName = "Host Pool Name"
+$flag = "0"
+$subscriptionID = "Subscription ID"
+
+Login-AzAccount
+
+Select-AzSubscription -Subscription $subscriptionID
+
+
+$sessionHosts = Get-AzWvdSessionHost -HostPoolName $hostPoolName -ResourceGroupName $hostPoolRG
+
+foreach ($sessionHost in $sessionHosts)
+{
+
+    if ($sessionHost.AssignedUser -eq $email)
+    {
+        $vmResourceID = $sessionHost.ResourceId
+        $vmName = $vmResourceID.Substring(128)
+        $flag = "1"
+        
+        Write-Output "Iniciando máquina virtual... Aguarde 5 minutos e tente conectar."
+        Stop-AzVM -Name $vmName -ResourceGroupName $hostPoolRG -Force -AsJob | Out-Null
+    }
+}
+
+if ($flag -eq "0")
+{
+
+        Write-Output "O usuário $email não possui máquina disponível no WVD."
+}
